ab13bbc6c3447a7498afea81bf521ec97c98c161
史ios <542235666@qq.com>
2016-12-13 23:14:53 +0800

    [web】【新建】js学习

---
+++ JavaScript/.idea/JS1/myjs.js
@@ -0,0 +1,20 @@
+/**
+ * Created by shiios on 16/12/13.
+ */
+
+// document.write("<h1>hello jikexueyuan1111</h1>");
+// document.getElementById("pid").innerHTML="huanleshijie";
+
+var string = "hellow";
+var i1 = 10;
+var flag = true;
+var arr = [1,2,3,4];
+var n = null;
+// var dic = ["a":"1","b":"2"];
+
+document.write(string + "<br/>");
+document.write(i1 + "<br/>");
+document.write(flag + "<br/>");
+document.write(arr + "<br/>");
+document.write(n + "<br/>");
+// document.write(dic);
