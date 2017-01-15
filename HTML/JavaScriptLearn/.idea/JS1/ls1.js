32fd3b641679553c8f0ea4ec03f85f3e950a8a5b
史ios <542235666@qq.com>
2017-01-15 22:55:14 +0800

    【新增】 html5新增结构元素

---
+++ HTML/JavaScriptLearn/.idea/JS1/ls1.js
@@ -0,0 +1,10 @@
+/**
+ * Created by shiios on 16/10/1.
+ */
+function mysum() {
+    var i = 10;
+    var j = 10;
+    var m = i * j;
+    document.getElementById("sumid").innerHTML=m;
+    // <br/>
+}
---
+++ HTML/JavaScriptLearn/.idea/JS2/JS2.js
@@ -0,0 +1,14 @@
+/**
+ * Created by shiios on 16/10/9.
+ */
+//
+
+function demo(a,b) {
+    var sum = a + b;
+
+    alert(sum);
+
+    return sum;
+}
+// var v1 = demo(10,30);
+// alert(v1);
